f64515b6340a9fec22b35f092ad8de4a21db3b92
Faizaan shaikh <sfaizan974@gmail.com>
2022-02-22 23:40:07 +0530

    Added routes and download page

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
+// const { engine, path } = require("express/lib/application");
 const connectDB = require("./config/db");
+const path = require("path");
 const PORT = process.env.PORT || 3002;
 const app = express();
+
+app.use(cors());
+app.use(express.static("public"));
 
 // mongo Connection
 connectDB();
 
+// Template engine
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
 // Routes
 app.use("/api/files", require("./routes/files"));
+app.use("/files", require("./routes/show"));
+app.use("/files/download", require("./routes/download"));
+
 app.get("/", (req, res) => {
   res.json({ message: "Server Up and running" });
 });
